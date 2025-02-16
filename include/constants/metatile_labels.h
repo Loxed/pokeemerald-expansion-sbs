ab4b95ec4aa7ee8e6464d78ba72888a6fecf496d
prison_lox <leopold@rombaut.org>
2025-02-16 04:03:09 +0100

    feat: added gym design to general tileset

--- include/constants/metatile_labels.h
+++ include/constants/metatile_labels.h
@@ -210,7 +210,7 @@
 #define METATILE_General_CalmWater             0x170
 #define METATILE_General_CaveEntrance_Bottom   0x0A7
 #define METATILE_General_CaveEntrance_Left     0x09F
-#define METATILE_General_Cave_Entrance_Right   0x02A
+#define METATILE_General_CaveEntrance_Right    0x02A
 #define METATILE_General_Door                  0x021
 #define METATILE_General_Door_Contest          0x1DB
 #define METATILE_General_Door_Gym              0x1CD
